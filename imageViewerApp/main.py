850c0b61355fbd01681b25a7027b49355368b087
akarshan2006 <akarshanghawri@gmail.com>
2024-11-16 01:02:19 +0530

    added a status bar

--- imageViewerApp/main.py
+++ imageViewerApp/main.py
@@ -5,11 +5,11 @@ root = Tk()
 root.title("Image Viewer App")  # title of the window
 
 # Load images and add them to an image list
-my_img1 = ImageTk.PhotoImage(Image.open("imageViewerApp/img/img1.png"))
-my_img2 = ImageTk.PhotoImage(Image.open("imageViewerApp/img/img2.png"))
-my_img3 = ImageTk.PhotoImage(Image.open("imageViewerApp/img/img3.png"))
-my_img4 = ImageTk.PhotoImage(Image.open("imageViewerApp/img/img4.png"))
-my_img5 = ImageTk.PhotoImage(Image.open("imageViewerApp/img/img5.png"))
+my_img1 = ImageTk.PhotoImage(Image.open("GUI/imageViewerApp/img/img1.png"))
+my_img2 = ImageTk.PhotoImage(Image.open("GUI/imageViewerApp/img/img2.png"))
+my_img3 = ImageTk.PhotoImage(Image.open("GUI/imageViewerApp/img/img3.png"))
+my_img4 = ImageTk.PhotoImage(Image.open("GUI/imageViewerApp/img/img4.png"))
+my_img5 = ImageTk.PhotoImage(Image.open("GUI/imageViewerApp/img/img5.png"))
 
 image_list = [my_img1, my_img2, my_img3, my_img4, my_img5]
 
@@ -23,13 +23,22 @@ def forward(image_number):
     global button_forward
     global button_back
 
-    # Remove the current image
-    my_label.grid_forget()
+    my_label.grid_forget()  # to remove the current image so that the images don't overlap
 
     # Update the label and button commands
     my_label = Label(image=image_list[image_number - 1])
     button_forward = Button(root, text=">>", command=lambda: forward(image_number + 1))
     button_back = Button(root, text="<<", command=lambda: back(image_number - 1))
+
+    # Update the status bar
+    status = Label(
+        root,
+        text=f"Image {image_number} of {len(image_list)}",
+        bd=1,
+        relief=SUNKEN,
+        anchor=E
+    )
+    status.grid(row=2, column=0, columnspan=3, sticky=W+E)
 
     # Disable forward button at the last image
     if image_number == len(image_list):
@@ -46,13 +55,22 @@ def back(image_number):
     global button_forward
     global button_back
 
-    # Remove the current image
-    my_label.grid_forget()
+    my_label.grid_forget()  # to remove the current image so that the images don't overlap
 
     # Update the label and button commands
     my_label = Label(image=image_list[image_number - 1])
     button_forward = Button(root, text=">>", command=lambda: forward(image_number + 1))
     button_back = Button(root, text="<<", command=lambda: back(image_number - 1))
+
+    # Update the status bar
+    status = Label(
+        root,
+        text=f"Image {image_number} of {len(image_list)}",
+        bd=1,
+        relief=SUNKEN,
+        anchor=E
+    )
+    status.grid(row=2, column=0, columnspan=3, sticky=W+E)
 
     # Disable back button at the first image
     if image_number == 1:
@@ -65,12 +83,22 @@ def back(image_number):
 
 # Initial button setup
 button_back = Button(root, text="<<", command=back, state=DISABLED)  # Back button starts as disabled
-button_exit = Button(root, text="Close App", command=root.quit)       # Exit button to close the app
+button_exit = Button(root, text="Close App", command=root.quit)      # Exit button to close the app
 button_forward = Button(root, text=">>", command=lambda: forward(2))  # Forward button to go to the next image
 
 # Display buttons
 button_back.grid(row=1, column=0)
 button_exit.grid(row=1, column=1)
-button_forward.grid(row=1, column=2)
+button_forward.grid(row=1, column=2, pady=10)
+
+# Adding status bar
+status = Label(
+    root,
+    text="Image 1 of 5",
+    bd=1,
+    relief=SUNKEN,
+    anchor=E
+)
+status.grid(row=2, column=0, columnspan=3, sticky=W+E)
 
 root.mainloop()
